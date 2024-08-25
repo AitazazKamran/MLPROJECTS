e5c25eaf022cfac6e3c6eb3caf58b5bb740640b1
AITAZAZ <Aitazazkamran11@gmail.com>
2024-08-25 16:15:21 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Aitazaz',
+    author_email='Aitazazkamran11@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn'],
+)
